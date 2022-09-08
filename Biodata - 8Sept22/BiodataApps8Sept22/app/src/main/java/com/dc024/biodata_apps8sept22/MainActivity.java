6358899d0eeade90e8dd4b48abdb8192b7307320
ryandhikabaa <ryandhikabintang0@gmail.com>
2022-09-08 21:27:03 +0700

    day1-biodata

---
+++ Biodata - 8Sept22/BiodataApps8Sept22/app/src/main/java/com/dc024/biodata_apps8sept22/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dc024.biodata_apps8sept22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
